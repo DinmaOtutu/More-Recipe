b4379783317ba825a99a945785976f08d6b7b8d7
ibukunolatunde1 <ibukunolatunde1@gmail.com>
2018-07-02 14:48:13 +0100

    Sign Up Page

---
+++ assets/signUp.js
@@ -0,0 +1,6 @@
+function switchToLogin() {
+   let  name = document.getElementById('inputName').value;
+   let email = document.getElementById('inputEmail').value;
+   let password = document.getElementById('inputPassword').value;
+   location.href = 'login.html';
+}
