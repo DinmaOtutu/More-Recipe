d7bc516cd0cd4fcdd002f6d42896e467c377e1b2
ibukunolatunde1 <ibukunolatunde1@gmail.com>
2018-07-02 15:30:28 +0100

    Edited to route to all recipes

--- assets/signUp.js
+++ assets/signUp.js
@@ -2,5 +2,19 @@ function switchToLogin() {
    let  name = document.getElementById('inputName').value;
    let email = document.getElementById('inputEmail').value;
    let password = document.getElementById('inputPassword').value;
-   location.href = 'login.html';
+   let data  = { name,email, password};
+   fetch('https://more-recipes.herokuapp.com/api/v1/users/signin', {
+       method: 'post',
+       body: JSON.stringify(data),
+       headers: {'Content-Type': 'application/json'}
+   }).then(res => res.status(201).json()).catch(err => console.log(err)).then(response => response.json());
+   location.href = 'allrecipes.html';
+}
+
+let check = function(){
+	if(document.getElementById("inputPassword").value == document.getElementById("rInputPassword").value){
+		document.getElementById("rInputPassword").style.borderColor = "#2ecc71";
+	}else{
+		document.getElementById("rInputPassword").style.borderColor = "red";
+	}
 }
