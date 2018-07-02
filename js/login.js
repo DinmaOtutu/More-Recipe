db1d34ccef62b5ddbfea795e6bf1b7a48d671822
kleva-j <kasmickleva@gmail.com>
2018-07-02 15:32:39 +0100

    modify: comfigure fetch function

--- js/login.js
+++ js/login.js
@@ -6,15 +6,30 @@ const getDomElem = () => {
     submitButton.addEventListener('click', () => {
         const email = emailDom.value;
         const password = passwdDom.value;
+        const user = { email, password }
+        fetch('https://more-recipes.herokuapp.com/api/v1/users/login', {
+            method: POST,
+            body: JSON.stringify(data),
+            header: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .catch(err => console.log(err))
+        .then(response => console.log(response))
     })
 
-    fetch
+}
+
+const initLogin = () => {
+    await 
 }
 
 
 
 window.onload = function(){
     getDomElem();
+    initLogin();
 }
 
 //post login details
